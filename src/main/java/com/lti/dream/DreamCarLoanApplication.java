636a5a0573c2256b21100af91adb2f95f9bde500
YashMakwana <yashmak28@gmail.com>
2022-11-27 19:27:55 +0530

    comm

---
+++ src/main/java/com/lti/dream/DreamCarLoanApplication.java
@@ -0,0 +1,14 @@
+package com.lti.dream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class DreamCarLoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamCarLoanApplication.class, args);
+	}
+
+}
