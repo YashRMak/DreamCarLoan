edc3404aa98c568765298dd59ce56f5d637b3aae
YashMakwana <yashmak28@gmail.com>
2022-11-29 11:28:56 +0530

    relations

--- src/main/java/com/lti/dream/beans/User.java
+++ src/main/java/com/lti/dream/beans/User.java
@@ -3,6 +3,7 @@ package com.lti.dream.beans;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
--- src/main/java/com/lti/dream/dao/UserDao.java
+++ src/main/java/com/lti/dream/dao/UserDao.java
@@ -17,4 +17,6 @@ public interface UserDao {
 	public List<User> getAllUser();
 
 	public User deleteUser(int userId);
+	
+	
 }
